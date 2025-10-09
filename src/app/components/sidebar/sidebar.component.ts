e0468f94ffd497c67881699ae6131020928ac023
tavershimafx <tmp11531@firstbanknigeria.com>
2025-10-09 16:41:59 +0100

    show sidebar items

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,41 +10,41 @@ export class SidebarComponent {
 
   sidebarItems: ISidebarItem[] = [
     { name: "Dashboard", iconClass: "apps", url: "/dashboard" },
-    // { name: "Projects", iconClass: "check_box", url: "/dashboard",
-    //   children: [
-    //     { name: "Tasks", iconClass: "apps", url: "/dashboard/tasks" },
-    //     { name: "Message", iconClass: "apps", url: "/dashboard/tasks/messages" },
-    //     { name: "Goals", iconClass: "apps", url: "/dashboard/tasks/goals" }
-    //   ]
-    //  },
-    // { name: "Ecommerce", iconClass: "shopping_cart", url: "dashboard",
-    //   children:[
-    //     { name: "Analytics", iconClass: "apps", url: "/dashboard/ecommerce" },
-    //     { name: "Orders", iconClass: "apps", url: "/dashboard/ecommerce/orders" },
-    //     { name: "Products", iconClass: "apps", url: "/dashboard/ecommerce/products" },
-    //     { name: "Customers", iconClass: "apps", url: "/dashboard/ecommerce/customers" }
-    //   ]
-    //  },
-    // { name: "Mail", iconClass: "mail", url: "dashboard",
-    //   children:[
-    //     { name: "Inbox", iconClass: "apps", url: "dashboard" },
-    //     { name: "Sent Mail", iconClass: "apps", url: "dashboard" },
-    //     { name: "Favorites", iconClass: "apps", url: "dashboard" },
-    //     { name: "Drafts", iconClass: "apps", url: "dashboard" },
-    //     { name: "Spam", iconClass: "apps", url: "dashboard" },
-    //     { name: "Trash", iconClass: "apps", url: "dashboard" }
-    //   ]
-    //  },
-    // { name: "Work", iconClass: "work", url: "dashboard",
-    //   children: [
-    //     { name: "Overview", iconClass: "apps", url: "dashboard" },
-    //     { name: "Job Search", iconClass: "apps", url: "dashboard" },
-    //     { name: "Schedule", iconClass: "apps", url: "dashboard" },
-    //     { name: "History", iconClass: "apps", url: "dashboard" },
-    //     { name: "Events", iconClass: "apps", url: "dashboard" },
-    //     { name: "Message", iconClass: "apps", url: "dashboard" }
-    //   ]
-    //  },
+    { name: "Projects", iconClass: "check_box", url: "/dashboard",
+      children: [
+        { name: "Tasks", iconClass: "apps", url: "/dashboard/tasks" },
+        { name: "Message", iconClass: "apps", url: "/dashboard/tasks/messages" },
+        { name: "Goals", iconClass: "apps", url: "/dashboard/tasks/goals" }
+      ]
+     },
+    { name: "Ecommerce", iconClass: "shopping_cart", url: "dashboard",
+      children:[
+        { name: "Analytics", iconClass: "apps", url: "/dashboard/ecommerce" },
+        { name: "Orders", iconClass: "apps", url: "/dashboard/ecommerce/orders" },
+        { name: "Products", iconClass: "apps", url: "/dashboard/ecommerce/products" },
+        { name: "Customers", iconClass: "apps", url: "/dashboard/ecommerce/customers" }
+      ]
+     },
+    { name: "Mail", iconClass: "mail", url: "dashboard",
+      children:[
+        { name: "Inbox", iconClass: "apps", url: "dashboard" },
+        { name: "Sent Mail", iconClass: "apps", url: "dashboard" },
+        { name: "Favorites", iconClass: "apps", url: "dashboard" },
+        { name: "Drafts", iconClass: "apps", url: "dashboard" },
+        { name: "Spam", iconClass: "apps", url: "dashboard" },
+        { name: "Trash", iconClass: "apps", url: "dashboard" }
+      ]
+     },
+    { name: "Work", iconClass: "work", url: "dashboard",
+      children: [
+        { name: "Overview", iconClass: "apps", url: "dashboard" },
+        { name: "Job Search", iconClass: "apps", url: "dashboard" },
+        { name: "Schedule", iconClass: "apps", url: "dashboard" },
+        { name: "History", iconClass: "apps", url: "dashboard" },
+        { name: "Events", iconClass: "apps", url: "dashboard" },
+        { name: "Message", iconClass: "apps", url: "dashboard" }
+      ]
+     },
     { name: "Monitoring", iconClass: "add_moderator", url: "dashboard",
       children: [
         { name: "Overview", iconClass: "apps", url: "/dashboard/uba/access-index" },
@@ -56,7 +56,7 @@ export class SidebarComponent {
       ]
      },
     { name: "Account", iconClass: "account_circle", url: "/dashboard/account" },
-    //{ name: "Gallery", iconClass: "account_circle", url: "/dashboard/account/gallery" },
+    { name: "Gallery", iconClass: "account_circle", url: "/dashboard/account/gallery" },
   ]
     constructor(){
     
